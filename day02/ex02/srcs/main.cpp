81a5578d9512250c09a95cf9eaa531b4575f56a3
LOSSY Roméo <rlossy@student.42.fr>
2019-12-06 16:46:10 +0100

    day02/ex02 done!

---
+++ day02/ex02/srcs/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.cpp                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/12/06 15:56:43 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2019/12/06 15:56:43 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../includes/Fixed.hpp"
+
+int 	main()
+{
+	Fixed   a;
+	Fixed   const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+
+	return (0);
+}
