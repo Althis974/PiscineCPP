e7bd5cba2a3424318a209f4d044a6b0c58f90e9b
LOSSY Roméo <rlossy@student.42.fr>
2020-01-05 23:54:36 +0100

    day07/ex00 done!

---
+++ day07/ex00/srcs/whatever.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   whatever.cpp                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/05 23:30:59 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/05 23:30:59 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template <typename T>
+const T &			min(const T &a, const T &b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+const T &			max(const T &a, const T &b)
+{
+	return (a > b ? a : b);
+}
+
+template <typename T>
+void				swap(T &a, T &b)
+{
+	T c;
+
+	c = a;
+	a = b;
+	b = c;
+}
+
+int					main()
+{
+	int   a = 2;
+	int   b = 3;
+
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
+	return (0);
+}
+
