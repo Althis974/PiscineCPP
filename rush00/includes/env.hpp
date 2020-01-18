e8d485f6205325de3ab36cb44f94f969324229a4
LOSSY Roméo <rlossy@student.42.fr>
2020-01-18 17:35:04 +0100

    Create env.hpp to avoid diamond inheritance and share pos struct

---
+++ rush00/includes/env.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   env.hpp                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/18 12:07:23 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/18 12:07:23 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef ENV_HPP
+#define ENV_HPP
+
+#include <string>
+#include <iostream>
+#include <ncurses.h>
+
+typedef struct				s_pos
+{
+	int 					x;
+	int 					y;
+}							t_pos;
+
+#endif //ENV_HPP
