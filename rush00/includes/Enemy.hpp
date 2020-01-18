64713955cde7c551ae8f6d9e3d395b3a72e41d4c
LOSSY Roméo <rlossy@student.42.fr>
2020-01-18 17:33:37 +0100

    create Enemy class, inherits from Entity

---
+++ rush00/includes/Enemy.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   Enemy.hpp                                        .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/18 12:47:48 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/18 12:47:48 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef ENEMY_HPP
+#define ENEMY_HPP
+
+#include "Entity.hpp"
+
+class Enemy : public Entity
+{
+
+public:
+
+	// Constructors
+	Enemy();
+	Enemy(t_pos pos);
+
+	// COpy constructor
+	Enemy(const Enemy &src);
+
+	// Destructor
+	virtual ~Enemy();
+
+	// Assignation operator overload
+	Enemy &			operator=(const Enemy &rhs);
+
+	// Moves
+	int             movement(t_pos map);
+
+	// Check if enemy is alive
+	bool			isAlive();
+};
+
+
+#endif //ENEMY_HPP
---
+++ rush00/srcs/Enemy.cpp
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   Enemy.cpp                                        .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/18 12:47:40 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/18 12:47:40 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../includes/Enemy.hpp"
+
+// Constructor
+
+Enemy::Enemy() : Entity()
+{
+
+}
+
+Enemy::Enemy(t_pos pos) : Entity(pos)
+{
+
+}
+
+Enemy::Enemy(const Enemy &src) : Entity(src)
+{
+
+}
+
+// Destructor
+
+Enemy::~Enemy()
+{
+
+}
+
+Enemy &			Enemy::operator=(const Enemy &rhs)
+{
+	Entity::operator=(rhs);
+
+	return (*this);
+}
+
+// Moves
+
+int				Enemy::movement(t_pos map)
+{
+	if (this->_pos.x < map.x)
+			moveRight();
+	else if (this->_pos.y < (map.y - 2))
+	{
+		moveDown();
+		this->_pos.x = 3;
+	}
+	else
+	{
+		this->_lives = 0;
+
+		mvprintw(this->_pos.y, this->_pos.x, " ");
+
+		return (1);
+	}
+
+	return (0);
+}
+
+// Check if enemy is alive
+
+bool			Enemy::isAlive()
+{
+	return (this->getLives() > 0);
+}
