3dde4d3254bce859195a5ec641a976eecb51080f
LOSSY Roméo <rlossy@student.42.fr>
2020-01-19 10:32:27 +0100

    Add keycodes

---
+++ rush00/includes/keycode.hpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   keycode.hpp                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: rlossy <rlossy@student.le-101.fr>          +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/19 10:26:19 by rlossy       #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/19 10:26:19 by rlossy      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef KEYCODE_HPP
+#define KEYCODE_HPP
+
+# define ONE '1'
+# define TWO '2'
+# define THREE '3'
+# define FOUR '4'
+# define FIVE '5'
+# define SIX '6'
+# define SEVEN '7'
+# define EIGHT '8'
+# define NINE '9'
+
+# define A 'a'
+# define B 'b'
+# define C 'c'
+# define D 'd'
+# define E 'e'
+# define F 'f'
+# define G 'g'
+# define H 'h'
+# define I 'i'
+# define J 'j'
+# define K 'k'
+# define L 'l'
+# define M 'm'
+# define N 'n'
+# define O 'o'
+# define P 'p'
+# define Q 'q'
+# define R 'r'
+# define S 's'
+# define T 't'
+# define U 'u'
+# define V 'v'
+# define W 'w'
+# define X 'x'
+# define Y 'y'
+# define Z 'z'
+
+# define ESC 27
+# define SP ' '
+
+# define F1 265
+# define F2 266
+# define F3 267
+# define F4 268
+# define F5 269
+# define F6 270
+# define F7 271
+# define F8 272
+# define F9 273
+# define F10 274
+# define F11 275
+# define F12 276
+
+
+#endif //KEYCODE_HPP
